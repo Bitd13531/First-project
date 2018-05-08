08f438bb25d364afe929149b98a97e733bfee623
Angel Villasmil <anvill_2002@hotmail.com>
2018-05-08 19:11:26 -0300

    Cambios mayores responsive a todo el sitio falta oficinas y boton mnu

--- form__send.php
+++ form__send.php
@@ -9,7 +9,7 @@ $telefono = $_POST['telefono'];
 $mensaje = $_POST['msj'];
 
 // Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
-$email_to = "anvill_2002@hotmail.com";
+$email_to = "atencion@mudanzasladino.tk";
 $email_subject = "Tienes un mensaje desde el sitio Web Mudanzas Ladino - Datos de contacto";
 
 // Aquí se deberían validar los datos ingresados por el usuario
@@ -23,16 +23,16 @@ echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
 die();
 }
 
+$email_message = "Hola, has recibido un nuevo mensaje del Sitio Web Mudanzas Ladino\n\n";
 $email_message = "Detalles de Contacto del Cliente Interesado:\n\n";
 $email_message .= "Nombre: " . $_POST['name'] . "\n";
 $email_message .= "Correo: " . $_POST['email'] . "\n";
-$email_message .= "Teléfono: " . $_POST['telefono'] . "\n";
+$email_message .= "Telefono: " . $_POST['telefono'] . "\n";
 $email_message .= "Mensaje: " . $_POST['msj'] . "\n\n";
 $email_message .= "Enviado el " . date ('d/m/Y', time ());
 
 
 // Ahora se envía el e-mail usando la función mail() de PHP
-header("Content-type: text/html;charset=\"utf-8\"");
 $headers = 'From: '.$mail."\r\n".
 'Reply-To: '.$mail."\r\n" .
 'X-Mailer: PHP/' . phpversion();
